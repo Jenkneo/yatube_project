43c31e64ba6623302da67a35dc5d615a04b862a5
Svyatoslav <jenkneo.lal@gmail.com>
2022-09-12 23:51:46 +0400

    Created dynamic links

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index),
-    path('group_list/', views.group_list),
+    path('', views.index, name='index'),
+    path('group_list/', views.group_list, name='group_list'),
+    #path('', views.index),
+    #path('group_list/', views.group_list),
 ]
