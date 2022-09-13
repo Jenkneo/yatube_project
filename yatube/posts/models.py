c8a5f71e45931b4869e60ba3264a47ebf6f27fc7
Svyatoslav <jenkneo.lal@gmail.com>
2022-09-13 23:25:02 +0400

    Migrations have been created and applied

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 # Create your models here.
+class Post(models.Model):
+    text = models.TextField()
+
+    # параметр auto_now_add определяет, что в поле будет автоматически
+    # подставлено время и дата создания новой записи
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    # Тип: ForeignKey, ссылка на модель User
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='posts')
